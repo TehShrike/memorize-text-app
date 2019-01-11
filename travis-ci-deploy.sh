6037a80a31f16712b11752fa412f096dafb49726
Josh Duff <me@JoshDuff.com>
2019-01-10 18:33:24 -0600

    A slightly different deploy script?

--- travis-ci-deploy.sh
+++ travis-ci-deploy.sh
@@ -1,3 +1,6 @@
+git config user.email "me@JoshDuff.com"
+git config user.name "TravisCI script"
+git remote add ghp https://TehShrike:$GITHUB_TOKEN@github.com/TehShrike/memorize-text-app.git
 git checkout -b gh-pages
 git add public/build.* -f
-git push origin gh-pages
+git push ghp gh-pages
