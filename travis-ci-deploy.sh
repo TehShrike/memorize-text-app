9063765cb7ec184fe38001ed3a1f90399aa0ab8c
Josh Duff <me@JoshDuff.com>
2019-01-10 18:37:07 -0600

    FORCE PUSH

--- travis-ci-deploy.sh
+++ travis-ci-deploy.sh
@@ -3,4 +3,4 @@ git config user.name "TravisCI script"
 git remote add ghp https://TehShrike:$GITHUB_TOKEN@github.com/TehShrike/memorize-text-app.git
 git checkout -b gh-pages
 git add public/build.* -f
-git push ghp gh-pages
+git push ghp gh-pages -f
