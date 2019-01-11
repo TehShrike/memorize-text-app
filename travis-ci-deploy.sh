b803cfa7ba28d44acb71868370a9e790a2a581c1
Josh Duff <me@JoshDuff.com>
2019-01-10 18:42:15 -0600

    oh wait, I need to commit first

--- travis-ci-deploy.sh
+++ travis-ci-deploy.sh
@@ -4,4 +4,5 @@ git config user.email "me@JoshDuff.com"
 git config user.name "TravisCI script"
 git remote add ghp https://TehShrike:$GITHUB_TOKEN@github.com/TehShrike/memorize-text-app.git
 git add *
+git commit -m "auto-deploy"
 git push ghp master:gh-pages -f
