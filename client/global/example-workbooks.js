d065a5cb8461e37bb3d62be14597a8961f9a6118
Josh Duff <me@JoshDuff.com>
2019-01-15 16:53:20 -0600

    A global folder with a file containing example workbooks

---
+++ client/global/example-workbooks.js
@@ -0,0 +1,8 @@
+export default [{
+	name: `Good verses to memorize`,
+	key: `1C6EBjsS-FY6KPzKnHCVFEcpYy_Gh_bvAJWcma50Qwrw`,
+}, {
+	name: `Bible Acrostic`,
+	key: `1OX4eS10MxNQbgfMzIZx2IEeivq6dGOc_F8X3IraMNds`,
+	moreInfoUrl: `https://gist.github.com/TehShrike/54495891029b617dff4c31fb8f08251c`,
+}]
