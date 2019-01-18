2bf7860521d2f4c43188b1f00dcd9e94b7379a3d
Josh Duff <me@JoshDuff.com>
2019-01-18 13:50:33 -0600

    Fix a failing mv, and try to make it so Travis notices if anything fails during deployment

--- travis-ci-deploy.sh
+++ travis-ci-deploy.sh
@@ -1,21 +1,17 @@
-git config user.email "me@JoshDuff.com"
-git config user.name "TravisCI script"
-
-mkdir deploy
-cd deploy
-git init
-git remote add ghp https://TehShrike:$GITHUB_TOKEN@github.com/TehShrike/memorize-text-app.git
-git fetch ghp
-git checkout ghp/gh-pages
-git checkout -b gh-pages
-
-git rm *
-
-touch .nojekyll
-git add .nojekyll
-echo "memorizetext.com" > CNAME
-
-mv ../public/* .
-git add *
-git commit -m "auto-deploy"
-git push ghp gh-pages
+git config user.email "me@JoshDuff.com" \
+&& git config user.name "TravisCI script" \
+&& mkdir deploy \
+&& cd deploy \
+&& git init \
+&& git remote add ghp https://TehShrike:$GITHUB_TOKEN@github.com/TehShrike/memorize-text-app.git \
+&& git fetch ghp \
+&& git checkout ghp/gh-pages \
+&& git checkout -b gh-pages \
+&& git rm * \
+&& touch .nojekyll \
+&& git add .nojekyll \
+&& echo "memorizetext.com" > CNAME \
+&& mv -r ../public/* . \
+&& git add * \
+&& git commit -m "auto-deploy" \
+&& git push ghp gh-pages
