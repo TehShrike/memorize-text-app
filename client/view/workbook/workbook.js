f77bea56ab444f4fea46e4c5c99dbdd04ecf799d
Josh Duff <me@JoshDuff.com>
2019-01-18 14:41:28 -0600

    Use the shared example workbooks to block remembering the examples

--- client/view/workbook/workbook.js
+++ client/view/workbook/workbook.js
@@ -1,4 +1,7 @@
 import Workbook from './Workbook.html'
+import exampleWorkbooks from 'global/example-workbooks.js'
+
+const exampleSheetKeysSet = new Set(exampleWorkbooks.map(({ key }) => key))
 
 export default mediator => ({
 	name: `workbook`,
@@ -8,7 +11,7 @@ export default mediator => ({
 	async resolve(data, { key }) {
 		const workbook = await mediator.call(`getWorkbook`, key)
 
-		if (key !== `1C6EBjsS-FY6KPzKnHCVFEcpYy_Gh_bvAJWcma50Qwrw`) {
+		if (!exampleSheetKeysSet.has(key)) {
 			mediator.call(`rememberWorkbook`, key, workbook.name)
 		}
 
