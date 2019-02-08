c0fcb29b2ff4993384bf97fda2b383873ee2ad83
Elisarmor <ejsisgreat@gmail.com>
2019-02-08 17:50:40 -0600

    Make info a state child of workbook

--- client/view/workbook/info/info.js
+++ client/view/workbook/info/info.js
@@ -1,16 +1,7 @@
-import settings from './settings.html'
+import info from './info.html'
 
 export default mediator => ({
-	name: `settings`,
-	//defaultChild: `select-sheet`,
-	route: `settings/:key`,
-	template: settings,
-	async resolve(data, { key }) {
-        const workbook = await mediator.call(`getWorkbook`, key)
-
-		return {
-            workbook,
-            key,
-		}
-	},
+	name: `workbook.info`,
+	route: `info`,
+	template: info,
 })
