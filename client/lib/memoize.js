6d2c9ea4e11bd4aba8317421c2a7e63b0b1496a3
Elisarmor <ejsisgreat@gmail.com>
2019-03-15 17:19:00 -0500

    Add a recovery length to the memoize function

--- client/lib/memoize.js
+++ client/lib/memoize.js
@@ -1,13 +1,16 @@
 export default (fn) => {
 	let lastCallArgs = null;
 	let lastCallResult = null;
+	let recoveryLength = 1000;
+	let lastCallTime = 0;
 	return (...args) => {
 		const thisCallArgs = args.join();
-		if (thisCallArgs === lastCallArgs) {
+		if (thisCallArgs === lastCallArgs && lastCallTime + recoveryLength >= new Date().getTime()) {
 			return lastCallResult;
 		}else{
 			lastCallResult = fn(...args);
 			lastCallArgs = thisCallArgs;
+			lastCallTime = new Date().getTime();
 			return lastCallResult;
 		}
 	}
