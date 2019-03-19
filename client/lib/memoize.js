1f683c4c9a871542a96e06255d2ef7e82dff25a3
Elisarmor <ejsisgreat@gmail.com>
2019-03-19 11:07:25 -0500

    Remove the last test

--- client/lib/memoize.js
+++ client/lib/memoize.js
@@ -1,29 +1,16 @@
 export default (fn) => {
-	let storedArgs = [ ];
 	let lastCallArgs = null;
 	let lastCallResult = null;
 	let recoveryLength = 1000;
 	let lastCallTime = 0;
 	return (...args) => {
 		const thisCallArgs = args.map(arg => arg.replace(/,/g, '|comma|')).join();
-
-		var storedArgsMatch = false;
-		for (var argByOneIndex in storedArgs) {
-			if (storedArgs[argByOneIndex].args == thisCallArgs && storedArgs[argByOneIndex].time + recoveryLength >= new Date().getTime()) {
-				storedArgsMatch = true;
-			}
-		}
-
 		if ((thisCallArgs === lastCallArgs && lastCallTime + recoveryLength >= new Date().getTime()) || storedArgsMatch) {
 			return lastCallResult;
 		}else{
 			lastCallResult = fn(...args);
 			lastCallArgs = thisCallArgs;
 			lastCallTime = new Date().getTime();
-			storedArgs.push({
-				time: new Date().getTime(),
-				args: thisCallArgs
-			})
 			return lastCallResult;
 		}
 	}
