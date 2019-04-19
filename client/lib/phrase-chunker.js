cf5acc9071443a377beab34a249db19ec6158981
Elisarmor <ejsisgreat@gmail.com>
2019-04-19 09:09:35 -0500

    Make test succeed

--- client/lib/phrase-chunker.js
+++ client/lib/phrase-chunker.js
@@ -1,29 +1,35 @@
-const isPotentialChunkWord = (word, chunkBarriersSet) => chunkBarriersSet.has(word[word.length - 1])
+const isPotentialChunkWord = (word, chunkBarriers, scale) => chunkBarriers[scale] === word[word.length - 1];
 
 export default ({ words, chunkMin, chunkMax, chunkIdeal, chunkBarriers }) => {
-
-	const chunkBarriersSet = new Set(chunkBarriers);
 
 	const chunks = [ 0 ]
 	let lastWordNumber = 0
 	let bestChunkSizeSoFar = null
 	let index = 0
+	let scale = 0
 	const addChunk = chunkSize => {
 		const wordNumber = chunkSize + lastWordNumber
 		chunks.push(wordNumber)
 		bestChunkSizeSoFar = null
 		lastWordNumber = wordNumber
 		index = wordNumber - 1
+		scale = 0
 	}
 	for (index = 0; index < words.length; ++index) {
 		const word = words[index]
 		const currentChunkSize = index - lastWordNumber + 1
 
+		console.log("Index - " + index);
+
 		if (currentChunkSize >= chunkMax) {
-			addChunk(bestChunkSizeSoFar || chunkIdeal)
+			if (scale >= chunkBarriers.length - 1) addChunk(bestChunkSizeSoFar || chunkIdeal);
+			else {
+				scale++;
+				index = lastWordNumber;
+			}		
 		} else if (index === words.length - 1) {
 			addChunk(currentChunkSize)
-		} else if (isPotentialChunkWord(word, chunkBarriersSet)) {
+		} else if (isPotentialChunkWord(word, chunkBarriers, scale)) {
 			if (currentChunkSize >= chunkIdeal) {
 				addChunk(currentChunkSize)
 			} else if (currentChunkSize >= chunkMin) {
