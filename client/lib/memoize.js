c72dcc3655bd65efd7434e1ff71aa1c74f35569d
Josh Duff <me@JoshDuff.com>
2019-07-04 06:57:02 -0500

    Dynamic memoize time

--- client/lib/memoize.js
+++ client/lib/memoize.js
@@ -1,17 +1,21 @@
-export default fn => {
+export default (fn, cacheTimeMs) => {
 	let lastCallArgs = null
 	let lastCallResult = null
-	const recoveryLength = 1000
 	let lastCallTime = 0
+
 	return (...args) => {
 		const thisCallArgs = args.map(arg => arg.replace(/,/g, `|comma|`)).join()
-		if (thisCallArgs === lastCallArgs && lastCallTime + recoveryLength >= new Date().getTime()) {
-			return lastCallResult
-		} else {
+
+		const canUseCachedValue = lastCallTime
+			&& thisCallArgs === lastCallArgs
+			&& lastCallTime + cacheTimeMs >= new Date().getTime()
+
+		if (!canUseCachedValue) {
 			lastCallResult = fn(...args)
 			lastCallArgs = thisCallArgs
 			lastCallTime = new Date().getTime()
-			return lastCallResult
 		}
+
+		return lastCallResult
 	}
 }
--- client/lib/memoize.test.js
+++ client/lib/memoize.test.js
@@ -7,7 +7,7 @@ test(`Returns the old value if the arguments are the same`, t => {
 	const memoizedFunction = memoize(() => {
 		functionCallCount += 1
 		return functionCallCount === 1 ? `first call` : `wat`
-	})
+	}, 100)
 	t.equal(memoizedFunction(`a`, `a`), `first call`)
 	t.equal(memoizedFunction(`a`, `a`), `first call`)
 
@@ -16,24 +16,25 @@ test(`Returns the old value if the arguments are the same`, t => {
 
 test(`Calls the function again if the arguments are different from last time`, t => {
 	let functionCallCount = 0
-	const memoizedFunction = memoize(() => functionCallCount += 1)
+	const memoizedFunction = memoize(() => functionCallCount += 1, 100)
 	memoizedFunction(`a`, `a`)
 	memoizedFunction(`a`, `b`)
 	t.equal(functionCallCount, 2)
 })
 
-test(`Calls the function again after 1.01 seconds`, async t => {
+test(`Calls the function again after enough time has elapsed`, async t => {
+	const CACHE_TIME = 100
 	let functionCallCount = 0
-	const memoizedFunction = memoize(() => functionCallCount += 1)
+	const memoizedFunction = memoize(() => functionCallCount += 1, CACHE_TIME)
 	memoizedFunction(`a`, `a`)
-	await delay(1010)
+	await delay(CACHE_TIME + 100)
 	memoizedFunction(`a`, `a`)
 	t.equal(functionCallCount, 2)
 })
 
 test(`Calls the function again with confusing arguments`, t => {
 	let functionCallCount = 0
-	const memoizedFunction = memoize(() => functionCallCount += 1)
+	const memoizedFunction = memoize(() => functionCallCount += 1, 100)
 	memoizedFunction(`ab,c`, `1,23`)
 	memoizedFunction(`ab`, `c,1,23`)
 	memoizedFunction(`ab,c,1`, `23`)
